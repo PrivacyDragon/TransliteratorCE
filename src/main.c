347d3faa86fd55ca6696d9ccff5af3e806bf556c
Privacy_Dragon <privacy_dragon@tutanota.com>
2021-11-24 20:58:21 +0100

    Add files via upload
    Resized the font files.
    Added alphabets

--- src/main.c
+++ src/main.c
@@ -8,27 +8,44 @@
 #include "oghamfont.h"
 
 fontlib_font_t *futhark;
+fontlib_font_t *nyctograph;
 fontlib_font_t *ogham;
+fontlib_font_t *phoenician;
+fontlib_font_t *protoSinaitic;
 
 char *input(){
 	const char *chars = "\0\0\0\0\0\0\0\0\0\0\"wrmh\0\0?[vqlg\0\0:zupkfc\0 ytojeb\0\0xsnida\0\0\0\0\0\0\0\0";
 	uint8_t key, i = 0;
 	int x = 1;
-	char buffer[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+	int y = 15;
+	char buffer[110];//= "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
 	delay(100);
-	/*for (int j = 0; j < 50; j++){
-		buffer[j] = "\"";
-	}*/
-	while((key = os_GetCSC()) != sk_Enter) {
+	for (int j = 0; j < 110; j++){
+		buffer[j] = 0;
+		//buffer++;
+	}
+	while((key = os_GetCSC()) != sk_Enter && i < 109) {
 		if(chars[key] && key != sk_Enter) {
+			if (key == 56){ //Delete key
+				//Delete the last character.
+				//This is done by going back one character. For what the user sees, the last character is replaced by a space.
+				i--;
+				gfx_PrintChar(' ');
+				x-=10;
+			}
 			buffer[i++] = chars[key];
-			gfx_SetTextXY(x+=5,15);
+			if (x >= LCD_WIDTH){
+				x = 1;
+				y+=15;
+			}
+			gfx_SetTextXY(x+=10,y);
 			gfx_PrintChar(chars[key]);
 		}
 	}
+	//buffer[i++] = 0;
 	//char *string = strtok(buffer, "\"");
-	gfx_PrintStringXY(buffer, 20, 20);
-	delay(1000);
+	//gfx_PrintStringXY(buffer, 20, 20);
+	//delay(1000);
 	return buffer;
 }
 
@@ -47,19 +64,22 @@ void th(const char *in, char *out){
 
 void SetupFonts(){
 	futhark = fontlib_GetFontByIndex("FUTHARK", 0);
+	nyctograph = fontlib_GetFontByIndex("NYCTO", 0);
 	ogham = fontlib_GetFontByIndex("OGHAM", 0);
+	phoenician = fontlib_GetFontByIndex("PHOENIC", 0);
+	protoSinaitic = fontlib_GetFontByIndex("SINAITIC", 0);
 }
 
 
 void Transliterate(fontlib_font_t *font){
 	char *toConvert;
-	char ToConvert[50];
+	char ToConvert[110];
 	gfx_BlitScreen();
 	gfx_FillScreen(255);
 	gfx_PrintStringXY("Input string:",1,1);
 	toConvert = input();
 	if (font == futhark){
-		gfx_PrintStringXY("function will enter", 30, 34);
+		//gfx_PrintStringXY("function will enter", 30, 34);
 		delay(1000);
 		th(toConvert, ToConvert);
 	} else {
@@ -69,7 +89,7 @@ void Transliterate(fontlib_font_t *font){
 		ToConvert[strlen(toConvert)] = 0;
 	}	
 	gfx_FillScreen(255);
-	gfx_PrintStringXY(ToConvert, 1, 25);
+	//gfx_PrintStringXY(ToConvert, 1, 25);
 	fontlib_SetFont(font, 0);
 	fontlib_SetCursorPosition(1,1);
 	fontlib_DrawString(ToConvert);
@@ -85,30 +105,78 @@ void menu() {
 	gfx_HorizLine(1, 26, LCD_WIDTH);
 	gfx_PrintStringXY("Menu:", LCD_WIDTH/2-10, 51);
 	gfx_PrintStringXY("[1]: Elder Futhark", 1, 71);
-	gfx_PrintStringXY("[2]: Ogham", 1, 91);
-	gfx_PrintStringXY("press [clear] to exit", 1, 126);
+	gfx_PrintStringXY("[2]: Nyctograph", 1, 91);
+	gfx_PrintStringXY("[3]: Ogham", 1, 111);
+	gfx_PrintStringXY("[4]: Phoenician", 1, 131);
+	gfx_PrintStringXY("[5]: Proto-sinaitic", 1, 151);
+	gfx_PrintStringXY("press [clear] to exit", 1, 176);
 	gfx_PrintStringXY("(c) Privacy_Dragon 2021", 1, LCD_HEIGHT-20);
 	do {
 		kb_Scan();
 		if (kb_Data[3] == kb_1){ //Elder Futhark
 			if (!futhark){
+				gfx_BlitScreen();
 				gfx_FillScreen(255);
 				gfx_PrintStringXY("ERROR: Font \"FUTHARK\" not found!", 1,1);
 				gfx_PrintStringXY("Transfer FUTHARK.8xv to your calculator!",1,16);
+				os_GetCSC();
+				gfx_BlitBuffer();
 			}
 			else {
 				Transliterate(futhark);
 			}
 		}
-		else if (kb_Data[4] == kb_2){ //Ogham
-			/*if (!ogham){
+		else if (kb_Data[4] == kb_2){ //Nyctograph
+			if (!nyctograph){
+				gfx_BlitScreen();
+				gfx_FillScreen(255);
+				gfx_PrintStringXY("ERROR: Font \"NYCTO\" not found!", 0,0);
+				gfx_PrintStringXY("Please transfer NYCTO.8xv to your calculator!",0,15);
+				os_GetCSC();
+				gfx_BlitBuffer();
+			}
+			else {
+				Transliterate(nyctograph);
+			}
+		}
+		else if (kb_Data[5] == kb_3){ //Ogham
+			if (!ogham){
+				gfx_BlitScreen();
 				gfx_FillScreen(255);
 				gfx_PrintStringXY("ERROR: Font \"OGHAM\" not found!", 0,0);
 				gfx_PrintStringXY("Please transfer OGHAM.8xv to your calculator!",0,15);
+				os_GetCSC();
+				gfx_BlitBuffer();
 			}
-			else {*/
-				Transliterate(ogham_font);
-			//}
+			else {
+				Transliterate(ogham);
+			}
+		}
+		else if (kb_Data[3] == kb_4){ //Phoenician
+			if (!protoSinaitic){
+				gfx_BlitScreen();
+				gfx_FillScreen(255);
+				gfx_PrintStringXY("ERROR: Font \"PHOENIC\" not found!", 0,0);
+				gfx_PrintStringXY("Please transfer PHOENIC.8xv to your calculator!",0,15);
+				os_GetCSC();
+				gfx_BlitBuffer();
+			}
+			else {
+				Transliterate(phoenician);
+			}
+		}
+		else if (kb_Data[4] == kb_5){ //Proto-sinaitic
+			if (!protoSinaitic){
+				gfx_BlitScreen();
+				gfx_FillScreen(255);
+				gfx_PrintStringXY("ERROR: Font \"SINAITIC\" not found!", 0,0);
+				gfx_PrintStringXY("Please transfer SINAITIC.8xv to your calculator!",0,15);
+				os_GetCSC();
+				gfx_BlitBuffer();
+			}
+			else {
+				Transliterate(protoSinaitic);
+			}
 		}
 		/*if (kb_Data[6] == kb_Clear){
 			return false;
